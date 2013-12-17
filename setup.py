fd14f41fee28e5c4a1704d1e01091d73d2b2d5d7
Eduard Carreras <ecarreras@gisce.net>
2013-12-17 16:23:12 +0100

    Setuptools is a recomendation to package

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='sepa',
@@ -9,7 +9,7 @@ setup(
     author='Grup El Gas, S.A.',
     author_email='informatica@el-gas.es',
     packages=['sepa'],
-    requires=['libComXML'],
+    install_requires=['libComXML'],
     license='GPLv3',
     description='SEPA',
     long_description=open('README').read(),
